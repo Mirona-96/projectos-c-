fb2ea68c3b905faed393516b42eb32a70f912ebe
Elton Mirona <99737578+Mirona-96@users.noreply.github.com>
2023-07-24 22:13:10 +0200

    Add files via upload

---
+++ Conta Bancária/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Globalization;
+
+class Conta
+{
+    public int _numeroConta { get; private set; }  
+    public string NomeTitular { get; set; }
+    public double SaldoConta { get; set; }
+    public int Nib { get; set; }
+
+    CultureInfo culture = CultureInfo.InvariantCulture;
+
+    public Conta (int numeroConta, string nomeTitular, double saldoConta, int nib)
+    {   _numeroConta = numeroConta;
+        NomeTitular = nomeTitular;
+        SaldoConta = saldoConta;
+        Nib= nib;
+    }
+
+    public Conta()
+    {
+
+    }
+
+    public void deposito (double valor)
+    {
+        SaldoConta += valor;
+        Console.WriteLine("\nSaldo: " + SaldoConta + " MZN");
+    }
+
+    public void saque (double valor)
+    {
+        double saldoInicial = SaldoConta;
+        double taxaSaque = 5.00; 
+        if (SaldoConta >= valor)
+        {
+            SaldoConta -= (valor + taxaSaque);
+            Console.WriteLine("Saldo da Conta: " + SaldoConta + " MZN");
+        } else
+        {
+            SaldoConta = saldoInicial;
+            Console.WriteLine("impossível realizar a operação" +
+                "\nSaldo da Conta: " + SaldoConta + " MZN");
+        }
+    }
+
+    public void transferencia (double valor, Conta contaDestino)
+    {
+        double saldoInicial = SaldoConta;
+        if (SaldoConta >= valor)
+        {
+            SaldoConta -= valor;
+            contaDestino.SaldoConta += valor;
+            Console.WriteLine("Operacao realizada com sucesso, montante tranferido para a conta" + contaDestino.Nib + ". \nSaldo da Conta: " + " MZN");
+        } else
+        {
+            SaldoConta = saldoInicial;
+            Console.WriteLine("Saldo insuficiente para realizar a transferência "
+                + "\nSaldo da Conta: " + SaldoConta + " MZN");
+        }
+    }
+
+    public void pgtoServico (double valor)
+    {
+        double saldoInicial = SaldoConta;
+        if (SaldoConta >= valor)
+        {
+            SaldoConta -= valor;
+            Console.WriteLine("Saldo da Conta: " + SaldoConta + " MZN");
+        } else
+        {
+            SaldoConta = saldoInicial;
+            Console.WriteLine("Saldo insuficiente para realizar o pagamento do servico" 
+                + "\nSaldo da Conta: " + SaldoConta + " MZN");
+        }
+
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine("\nnumero da conta: " + _numeroConta
+           + "\nTitular da conta: " + NomeTitular
+           + "\nNIB: " + Nib
+           + "\nSaldo da Conta: " + SaldoConta.ToString("F2", culture) + " MZN") ; 
+    }
+}
---
+++ Conta Bancária/Teste.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta_Bancária
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleat = new Random();
+            CultureInfo culture = CultureInfo.InvariantCulture;
+
+            int numeroConta = aleat.Next(10000, 99999); //atribui o numero de conta
+
+            Console.Write("\nInforme o nome do titular: ");
+            string nomeTitular = Console.ReadLine();
+
+            Console.Write("\nPretende fazer um  depósito inicial? (s/n): ");
+            char opcao = char.Parse(Console.ReadLine());
+            double deposito = 0;
+            double saldoConta = 0;
+
+            int nib = aleat.Next(10000000, 99999999); //atribui o nib
+
+            if (opcao == 's' || opcao == 's')
+            {
+             Console.Write("\nInsira o montante inicial a depositar: ");
+             deposito = double.Parse(Console.ReadLine(), culture);
+             saldoConta += deposito;
+            }             
+
+            Conta c = new Conta (numeroConta, nomeTitular, saldoConta, nib);
+            Console.WriteLine("Dados da Conta: ");
+            c.Imprimir();
+
+            do
+            {
+                Console.Write("\nPretende efectuar outra transacao? (s/n): ");
+                opcao = char.Parse(Console.ReadLine());
+
+                Console.WriteLine("Escolha a transacao: " +
+                    "\n1. Deposito;" +
+                    "\n2. Levantamento;" +
+                    "\n3. Transferencia;" +
+                    "\n4. Pagamentos de serviços" +
+                    "\n0. Sair");
+
+                int escolha = int.Parse(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1:
+                        Console.Write("\nInsira  o montante a depositar: ");
+                        deposito = double.Parse(Console.ReadLine(), culture);
+                        c.deposito(deposito);
+                        break;
+
+                    case 2:
+                        Console.Write("\nInsira o montante a sacar: ");
+                        double saque = double.Parse(Console.ReadLine(), culture);
+                        c.saque(saque);
+                        break;
+
+                    case 3:
+                        Conta contaDestino = new Conta();
+                        Console.Write("\nInsira o numero da conta destino: ");
+                        contaDestino.Nib = int.Parse(Console.ReadLine());
+                        Console.Write("\nInsira o montante a transferir: ");
+                        double valor = double.Parse(Console.ReadLine(), culture);
+                        c.transferencia(valor, contaDestino);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\nEscolha o serviço que pretende pagar: " +
+                            "\n1. Agua;" +
+                            "\n2. Energia;" +
+                            "\n3. TV e Internet");
+
+                        int servico = int.Parse(Console.ReadLine());
+
+                        switch (servico)
+                        {
+                            case 1:
+                                Console.WriteLine("Insira o montante a pagar: ");
+                                valor = double.Parse(Console.ReadLine(), culture);
+                                c.pgtoServico(valor);
+                                break;
+
+                            case 2:
+                                Console.WriteLine("Insira o montante a pagar: ");
+                                valor = double.Parse(Console.ReadLine(), culture);
+                                c.pgtoServico(valor);
+                                break;
+
+                            case 3:
+                                Console.WriteLine("Insira o montante a pagar: ");
+                                valor = double.Parse(Console.ReadLine(), culture);
+                                c.pgtoServico(valor);
+                                break;
+
+                            default:
+                                Console.WriteLine("opcao invalida!");
+                                break;
+                        }
+                        break;
+
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcao invalida!");
+                        break;
+                }
+
+            } while (opcao == 's' || opcao == 's');
+
+
+
+            Console.WriteLine("\nDados da conta actualizados!");
+        }
+    }
+}
